90c577ed4def430df7e7fc034127ef5a95da7d9a
KokoMoraDesigns <yo.kokomora@gmail.com>
2025-09-16 17:47:06 +0200

    newspaper-container file initial setup- without mysql database & newspaperitem

--- src/components/newspaper/newspaper-container.js
+++ src/components/newspaper/newspaper-container.js
@@ -5,14 +5,95 @@ import NewspaperItem from './newspaper-item';
 
 export default class NewspaperContainer extends Component {
 
+    constructor() {
+        super();
+
+        this.state = {
+            pageTitle: 'are you here for the gossip?',
+            isLoading: false,
+            data: []
+        };
+
+        this.handlePageTitleUpdate = this.handlePageTitleUpdate.bind(this);
+        this.handleFilter = this.handleFilter.bind(this);
+    }
+
+    handleFilter(filter) {
+        if (filter === 'CLEAR_FILTERS') {
+            this.getNewspaperItems();
+        
+        } else {
+            this.getNewspaperItems(filter);
+        }
+    }
+
+    getNewspaperItems(filter = null){
+        axios.get('')
+        .then(response => {
+            if (filter) {
+                this.setState({
+                    data: response.data.newspaper_items.filter(item => {
+                        return item.category === filter;
+                    })
+                });
+            } else {
+                this.setState({
+                    data: response.data.newspaper_items
+                });
+
+            }
+
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    }
+
+    newspaperItems() {
+        return this.state.data.map(item => {
+            return (
+                <NewspaperItem
+                    key={item.id}
+                    item={item}
+                />
+            );
+        });
+    }
+
+    componentDidMount() {
+        this.getNewspaperItems();
+    }
+
+    handlePageTitleUpdate() {
+        this.setState({
+            pageTitle: 'may your wishes come true'
+        });
+    }
+
     render() {
+
+        if (this.state.isLoading) {
+            return <div>Loading...</div>;
+        }
 
         return (
 
-           <div>
+            <div className='newspaper-wrapper'>
 
+                <div className='filter-links'>
+                    <button className='btn' onClick={() => this.handleFilter('Teachers')}>Teachers</button>
+                    <button className='btn' onClick={() => this.handleFilter('Classmates')}>Classmates</button>
+                    <button className='btn' onClick={() => this.handleFilter('Parents')}>Parents</button>
+                    <button className='btn' onClick={() => this.handleFilter('Everyone')}>Everyone</button>
+                </div>
 
-           </div>
+                <div className='newspaper-items-wrapper'>
+                    {this.newspaperItems()}
+                </div>
+                
+            </div>
+
+           
 
         );
     }
