ba4759a2382057a31bae52a4306640f6231581ff
KokoMoraDesigns <yo.kokomora@gmail.com>
2025-09-16 17:26:31 +0200

    newspaper-item file added

---
+++ src/components/newspaper/newspaper-container.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+import NewspaperItem from './newspaper-item';
+
+export default class NewspaperContainer extends Component {
+
+    render() {
+
+        return (
+
+           <div>
+
+
+           </div>
+
+        );
+    }
+}
