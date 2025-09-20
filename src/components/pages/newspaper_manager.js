9bb237a59dd9f2ffbaeb2603a0f27799fb043df9
KokoMoraDesigns <yo.kokomora@gmail.com>
2025-09-20 11:14:43 +0200

    habilitadas las funciones para actualizar y para eliminar noticias, añadido un spinner con mensaje de loading gossips

--- src/components/pages/newspaper_manager.js
+++ src/components/pages/newspaper_manager.js
@@ -15,18 +15,48 @@ export default class NewspaperManager extends Component {
             content: '',
             cover_image: '',
             category: '',
-            images: '', 
-            message: ''
+            images: '',
+            editingId: null,
+            message: '',
+            newsList: [],
+            loading: false
         };
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleEdit = this.handleEdit.bind(this);
+        this.handleDelete = this.handleDelete.bind(this);
 
     }
+
+
+    componentDidMount() {
+        this.loadNews();
+    }
+
+
+    loadNews() {
+        this.setState({ loading: true });
+
+        axios.get('http://localhost:5005/get_news', { withCredentials: true })
+        .then(response => {
+            this.setState({ 
+                newsList: response.data.newspaper_items,
+                loading:false 
+            });
+
+        })
+        .catch(error => {
+            console.log('loadNews error', error);
+            this.setState({ loading: false });
+        });
+    }
+
 
     handleChange(event) {
         this.setState({ [event.target.name]: event.target.value });
     }
+
 
     handleSubmit(event) {
         event.preventDefault();
@@ -40,20 +70,72 @@ export default class NewspaperManager extends Component {
             user_id: 1
         };
 
-        axios.post('http://localhost:5005/add_new', newData, { withCredentials: true })
-        .then(response => {
-            this.setState({
-                message: 'la noticia ha sido creada con éxito',
-                title: '',
-                content: '',
-                cover_image: '',
-                category: '',
-                images: ''
+        if (this.state.editingId) {
+            axios.put(`http://localhost:5005/update_news/${this.state.editingId}`, newData, {withCredentials: true} )
+            .then(() => {
+                this.setState({
+                    message: 'this gossip has been successfully updated',
+                    title: '',
+                    content: '',
+                    cover_image: '', 
+                    images: '',
+                    category: '',
+                    editingId: null
+                });
+                this.loadNews();
+            })
+            .catch(error => {
+                console.log('updateNews error', error);
+                this.setState({ message: 'there has been an error in the proccess of updating this gossip'});
             });
+        } else {
+
+            axios.post('http://localhost:5005/add_new', newData, { withCredentials: true })
+            .then(() => {
+                this.setState({
+                    message: 'this gossip has been successfully created',
+                    title: '',
+                    content: '',
+                    cover_image: '',
+                    category: '',
+                    images: ''
+                });
+                this.loadNews();
+            })
+            .catch(error => {
+                console.log('addNew error', error);
+                this.setState({ message: 'there has been an error in the proccess of spreading this gossip' });
+            });
+
+        }
+
+
+    }
+
+    handleEdit(news) {
+        this.setState({
+            editingId: news.id,
+            title: news.title,
+            content: news.content,
+            cover_image: news.cover_image,
+            images: news.images,
+            category: news.category
+        });
+    }
+
+
+
+    handleDelete(id) {
+        if (!window.confirm('are you sure you want to remove this gossip?')) return;
+
+        axios.delete(`http://localhost:5005/delete_news/${id}`, { withCredentials: true })
+        .then(() => {
+            this.setState({ message: 'this gossip has been successfully removed'});
+            this.loadNews();
         })
         .catch(error => {
-            console.log('addNew error', error);
-            this.setState({ message: 'error al crear esta noticia' });
+            console.log('deleteNews error', error);
+            this.setState({ message: 'there has been an error in the proccess of removing this gossip' });
         });
     }
 
@@ -64,19 +146,21 @@ export default class NewspaperManager extends Component {
         return (
             <div className='newspaper-manager'>
 
+                <div className='newspaper-manager-action'>{this.state.editingId ? 'Wanna update a rumour?' : 'Wanna create a rumour?'}</div>
+
                 <form onSubmit={this.handleSubmit}>
 
                     <input
                         type='text'
                         name='title'
-                        placeholder='title'
+                        placeholder='Title'
                         value={this.state.title}
                         onChange={this.handleChange}
                     />
 
                     <textarea
                         name='content'
-                        placeholder='the gossip'
+                        placeholder='The gossip'
                         value={this.state.content}
                         onChange={this.handleChange}
                     />
@@ -84,7 +168,7 @@ export default class NewspaperManager extends Component {
                     <input
                         type='text'
                         name='cover_image'
-                        placeholder='cover image URL'
+                        placeholder='Cover URL'
                         value={this.state.cover_image}
                         onChange={this.handleChange}
                     />
@@ -92,7 +176,7 @@ export default class NewspaperManager extends Component {
                     <input
                         type='text'
                         name='image'
-                        placeholder='evidences URL'
+                        placeholder='Evidences URL'
                         value={this.state.images}
                         onChange={this.handleChange}
                     />
@@ -107,11 +191,48 @@ export default class NewspaperManager extends Component {
                         <option value='Parent'>Parent</option>
                     </select>
 
-                    <button type='submit'>Publish</button>
+                    <button type='submit'> {this.state.editingId ? 'Ooops, I did it again' : 'May the chaos arise'}</button>
                 </form>
 
                 {this.state.message && <p>{this.state.message}</p>}
 
+                <div className='published-news'>
+                    {this.state.loading ? (
+                        <div className='loading-container'>
+                            <div className='spinner'></div>
+                            <div className='text'>Loading gossips...</div>
+                        </div>
+                    
+                    ) : (
+                        Array.isArray(this.state.newsList) && this.state.newsList.length > 0 ? (
+                            this.state.newsList.map(news => (
+
+                                <div className='published-new' key={news.id}>
+                                    <div className='title category'>
+                                        {news.title}: {news.category} 
+                                    </div>
+
+                                    {news.cover_image && (
+                                        <img
+                                            src={`http://localhost:5005${news.cover_image}`}
+                                            alt={news.title}
+                                            className='cover-thumb'
+                                        />
+                                    )}
+
+                                    <button className='btn' onClick={() => this.handleEdit(news)}> update gossip </button>
+                                    <button className='btn' onClick={() => this.handleDelete(news.id)}> bye gossip </button>
+                                </div>
+                            ))
+            
+
+                        ) : (
+
+                            <div className='text'>No gossips found</div>
+                        )
+                    )}
+
+                </div>
             </div>
         );
     }
