ed2c263dec52bc6385d2bec2f99a85c06f1518a2
KokoMoraDesigns <yo.kokomora@gmail.com>
2025-09-29 13:11:27 +0200

    centrar el contenido de missed me?

--- src/components/pages/missed-me.js
+++ src/components/pages/missed-me.js
@@ -18,29 +18,24 @@ export default function() {
 
 
             <div className='right-column'>
-                <p className='edit-paragraph'>Hi my loves,</p><p>Did you missed me?</p><p>The adrenaline of not knowing when were you being watched?</p> 
-                    <p>The euphoria of being the watchers?</p> <p>The shock of reading your own story in my body?</p> 
-                    The joy and the surprise of discovering your friends' secrets?<p>
-                    Well, that's a shame... I'm not gonna lie to you, I'm a little sad about your feelings towards me.</p><p>
-                    However, I don't need your sympathy in order to exist. You've been so naughty people that the accumulation 
-                    of all your karma has summoned me, so... hi bitches! I'm back! Back in 2007, Mother, Gossip Girl OG, was the 
+                <p className='edit-paragraph'>
+                    
+                    
+                    Hi my loves,</p><p>Did you missed me?</p><p>The adrenaline of not knowing when were you being watched?</p> 
+                    <p>The euphoria of being the watchers?</p> <p>The perturbing churning of emotions when reading in my body your own story?</p> 
+                    
+                    <p>Well, that's a shame, because... hi bitches! I'm back! </p> 
+                    <p>Back in 2007, Mother was the 
                     -slightly disturbed- diary of a man who contemplated from the periphery the world he so badly wanted to be a part of 
-                    -a man who years later would evolve into an actual psychopath-. Years later, in 2021, her Daughter, Gossip Girl 2.0, was 
-                    created by a tiny, rancorous & revenger group of teachers who wanted to feel some power in an elitist school. Am I being 
-                    nostalgic about my family? No, not at all, I am just saying the cosmos has decided that it's time for the magic to manifest 
-                    itself a third time, so she gave birth to a little sister for beautiful Gossip Girl 2.0: hi, I'm the problem, it's me!</p><p>
-                    I want us to be friends, so you can call me by my initials: GG.</p><p> And you may be wondering: if her mother helped a troubled 
-                    writer and her sister a bitter group of teachers... who is GG here for? Well... Duh! I am here for the baddies. For all those amazing 
-                    sinners who live a wild, free, self-loving and non-apologetic life, but have to return every Monday to school just to hear the malicious 
-                    whispers about them and to see the whisperers' two-faced faces, who sin in the darkness and criticize under the spotlight.</p><p>
-                    Well... in my family we hate hypocrisy. So let's do something about it, shall we? And by something I mean airing all your dirty little secrets, 
-                    so that next time you want to whisper about someone, you just encounter a shocked face claiming 'for real? Are you really trying to go down there?'.</p><p>
-                    I know, I know... you are angry at me, you hate me, you despise me, you want me to disappear, blah, blah, blah. But that's not gonna happen, and, in fact, 
-                    that's just your fear screaming, not your real feelings -I really think you're slightly excited to meet me-, so take a moment and make both of us a favor: 
-                    breath in for 4 seconds... now hold you breath for 7 seconds... and breath out for another 8 seconds. Hope you are feeling better now.</p><p> Also, in case you are
-                    still angry, before you give in to the urge to insult me, think about it this way: by the moment in which we say goodbye to each other and I go to sleep with 
-                    my mom and older sister... you will be a baddie too! Isn't it much more fun to be a free sinner than a fearful carper? How was it? I can't recall... Oh, yeah: 
-                    'good girls go to heaven, bad girls go everywhere'. So let's all sin under the spotlight together! I can promise you: you will be much happier.</p><p className='edit-paragraph'> Didn't you 
+                    -a man who years later would evolve into an actual psychopath-. Years later, in 2021, Gossip Girl 2.0, was 
+                    created by a tiny, rancorous & revenger group of teachers who wanted to feel some power in an elitist school... </p>
+                    <p> And you may be wondering: who is GG here for? Well... Duh... for <b>the baddies!</b> For all those amazing 
+                    sinners who live a wild, free, self-loving and non-apologetic life, but returns to school every Monday just to see the two-faced gossips who sin in secret and criticize under the spotlight.
+                    We hate hypocrisy in my family, so let's do something about it, shall we? And by something I mean airing all your dirty little secrets, 
+                    so that next time you want to whisper about someone else, you just encounter a shocked face claiming 'for real? Are you really trying to go down there?'.</p><p>
+                    I know, I know... you hate me, you despise me, you want me to disappear, blah, blah, blah. But that's not gonna happen, and, in fact, 
+                    maybe that's just your fear screaming, because I really think you're slightly excited to meet me, so take a moment and make both of us a favor: 
+                    breath in for 4 seconds... now hold you breath for 7 seconds... and breath out for another 8 seconds. Hope you are feeling better now.</p><p className='edit-paragraph'> Didn't you 
                     wanted to be the main character of your life? Welcome to the movie... </p>
 
                     <div className='logo'>
