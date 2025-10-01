fb22aa125e17d2667fb84f4a32aa430c06730635
KokoMoraDesigns <yo.kokomora@gmail.com>
2025-10-01 13:58:15 +0200

    modificada la url de .env.production y ubicado console.log en api.js para hacer comprobaciones de backend

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5005';
+console.log("API_URL usada:", API_URL);
 
 const api = axios.create({
     baseURL: API_URL,
